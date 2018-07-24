ccda02ca4cbe2af248533977cc2a69e7e60878a3
coolJenny <coolJenny009g@gmail.com>
2018-07-23 22:02:55 -0400

    ...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,55 @@
+jssor_1_slider_init = function() {
+
+  var jssor_1_options = {
+    $AutoPlay: 1,
+    $AutoPlaySteps: 5,
+    $SlideDuration: 160,
+    $SlideWidth: 200,
+    $SlideSpacing: 3,
+    $ArrowNavigatorOptions: {
+      $Class: $JssorArrowNavigator$,
+      $Steps: 5
+    },
+    $BulletNavigatorOptions: {
+      $Class: $JssorBulletNavigator$
+    }
+  };
+
+  var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+  /*#region responsive code begin*/
+
+  var MAX_WIDTH = 980;
+
+  function ScaleSlider() {
+      var containerElement = jssor_1_slider.$Elmt.parentNode;
+      var containerWidth = containerElement.clientWidth;
+
+      if (containerWidth) {
+
+          var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+          jssor_1_slider.$ScaleWidth(expectedWidth);
+      }
+      else {
+          window.setTimeout(ScaleSlider, 30);
+      }
+  }
+
+  ScaleSlider();
+
+  $Jssor$.$AddEvent(window, "load", ScaleSlider);
+  $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+  $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+  /*#endregion responsive code end*/
+};
+
+
+
+
+
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
