49826709d76affd7c04fafaaef1aabb073e6e46f
coolJenny <coolJenny009g@gmail.com>
2018-07-28 01:04:42 -0400

    ..

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var owl = $("#owl-demo");
  
   owl.owlCarousel({
-      items : 10, //10 items above 1000px browser width
+      items : 5, //5 items above 1000px browser width
       itemsDesktop : [1000,5], //5 items between 1000px and 901px
       itemsDesktopSmall : [900,3], // betweem 900px and 601px
       itemsTablet: [600,2], //2 items between 600 and 0
@@ -13,15 +13,8 @@ $(document).ready(function() {
   // Custom Navigation Events
   $(".next").click(function(){
     owl.trigger('owl.next');
-  })
+  });
   $(".prev").click(function(){
     owl.trigger('owl.prev');
-  })
-  $(".play").click(function(){
-    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  })
-  $(".stop").click(function(){
-    owl.trigger('owl.stop');
-  })
- 
+  });
 });
