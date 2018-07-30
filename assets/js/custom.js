0c9f88ce9802e67ad9424600b7603764afae7893
coolJenny <coolJenny009g@gmail.com>
2018-07-29 23:56:55 -0400

    ...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,14 +7,10 @@ $(document).ready(function() {
       itemsDesktop : [1000,5], //5 items between 1000px and 901px
       itemsDesktopSmall : [900,3], // betweem 900px and 601px
       itemsTablet: [600,2], //2 items between 600 and 0
-      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+      navigation : true
   });
  
-  // Custom Navigation Events
-  $(".next").click(function(){
-    owl.trigger('owl.next');
-  });
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
-  });
+  $('.owl-prev').html('<i class="fa fa-angle-left"></i>');
+  $('.owl-next').html('<i class="fa fa-angle-right"></i>');
 });
