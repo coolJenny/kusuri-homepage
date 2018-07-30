68011ed5e61720f6f38cafe1b388e63ce977e96f
coolJenny <coolJenny009g@gmail.com>
2018-07-30 06:37:19 -0400

    //

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
  
-  var owl = $("#owl-demo1");
- 
-  owl.owlCarousel({
+  var owl1 = $("#owl-demo1"); 
+  owl1.owlCarousel({
       items : 5, //5 items above 1000px browser width
       itemsDesktop : [1000,5], //5 items between 1000px and 901px
       itemsDesktopSmall : [900,3], // betweem 900px and 601px
@@ -13,4 +12,15 @@ $(document).ready(function() {
  
   $('.owl-prev').html('<i class="fa fa-angle-left"></i>');
   $('.owl-next').html('<i class="fa fa-angle-right"></i>');
+
+
+  var owl2 = $("#owl-demo2"); 
+  owl2.owlCarousel({
+      items : 6, //6 items above 1000px browser width
+      itemsDesktop : [1000,5], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,3], // betweem 900px and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+      navigation : true
+  });
 });
